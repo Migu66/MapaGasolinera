9712f45b07f74e125105236d9a4a99658cc18f9d
Migu66 <miguelgp789@gmail.com>
2023-11-07 13:48:35 +0100

    añadido el logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.mapagasolineras"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mapagasolineras"
