be283fc1bd4b5cb5f447faa4ce82e58f607156f5
David <davidandreiflorea16@gmail.com>
2023-11-10 09:44:58 +0100

    Mapa creado

--- app/src/main/java/com/example/mapagasolineras/MainActivity.java
+++ app/src/main/java/com/example/mapagasolineras/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mapagasolineras;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    EditText txtLatitud, txtLongitud;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLatitud = findViewById(R.id.txtLatitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng guadalajara = new LatLng(40.635479, -3.174281);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(guadalajara));
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        txtLatitud.setText("" + latLng.latitude);
+        txtLongitud.setText("" + latLng.longitude);
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        txtLatitud.setText("" + latLng.latitude);
+        txtLongitud.setText("" + latLng.longitude);
     }
 }
