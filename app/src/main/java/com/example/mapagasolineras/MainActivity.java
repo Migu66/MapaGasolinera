d5829f838e333e21a36293c7476ebbe108c3d573
Miguel <miguelgp789@gmail.com>
2023-12-12 17:54:38 +0100

    Final 2

--- app/src/main/java/com/example/mapagasolineras/MainActivity.java
+++ app/src/main/java/com/example/mapagasolineras/MainActivity.java
@@ -9,12 +9,17 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.mapagasolineras.interfaces.GasolineraAPI;
-import com.example.mapagasolineras.models.Gasolinera;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -32,21 +37,22 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import com.android.volley.RequestQueue;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    private TextView mTextViewResult;
+    private RequestQueue mQueue;
+
     SupportMapFragment supportMapFragment;
     FusedLocationProviderClient fusedLocationProviderClient;
     EditText txtLatitud, txtLongitud;
     GoogleMap mMap;
     String n = "19";
-    String latitud, longitud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,7 +83,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     }
 
                 }).check();
+
+
+        mTextViewResult = findViewById(R.id.text_view_result);
+        Button buttonParse = findViewById(R.id.button_parse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        buttonParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+
+
     }
+
+    private void jsonParse() {
+        String url = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroProvincia/19";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new com.android.volley.Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("ListaEESSPrecio");
+
+                    for(int i = 0; i < jsonArray.length(); i++){
+                        JSONObject gasolinera = jsonArray.getJSONObject(i);
+
+                        String latitudStr = gasolinera.getString("Latitud");
+                        String longitudStr = gasolinera.getString("Longitud (WGS84)");
+
+                        try {
+
+                            mTextViewResult.append((latitudStr + " " + longitudStr + " "));
+                            double latitud = Double.parseDouble(latitudStr);
+                            double longitud = Double.parseDouble(longitudStr);
+
+                            LatLng gasolineraLatLng = new LatLng(latitud, longitud);
+                            mMap.addMarker(new MarkerOptions().position(gasolineraLatLng));
+                        } catch (NumberFormatException e) {
+                            Log.e("GasolineraParsing", "Error parsing coordinates: " + e.getMessage());
+                        }
+                    }
+                } catch (JSONException e) {
+                    Log.e("GasolineraJSON", "Error parsing JSON: " + e.getMessage());
+                }
+            }
+        }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+                Log.e("GasolineraAPI", "Error fetching gas stations: " + error.getMessage());
+            }
+        });
+
+        mQueue.add(request);
+    }
+
 
     public void getCurrentLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -107,19 +171,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                         LatLng guadalajara = new LatLng(40.635479, -3.174281);
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(guadalajara));
 
-                        find(n);
-
-                        LatLng g1 = new LatLng(40.64390338991715, -3.142959697317834);
-                        googleMap.addMarker(new MarkerOptions().position(g1));
-
-                        LatLng g2 = new LatLng(40.63366404469122, -3.1702702570533714);
-                        googleMap.addMarker(new MarkerOptions().position(g2));
-
-                        LatLng g3 = new LatLng(40.63280456713761, -3.1828241483232027);
-                        googleMap.addMarker(new MarkerOptions().position(g3));
-
-                        LatLng g4 = new LatLng(40.63579518051684, -3.189177109407755);
-                        googleMap.addMarker(new MarkerOptions().position(g4));
 
                     }
                 });
@@ -147,47 +198,5 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(@NonNull GoogleMap googleMap) {
     }
 
-    public void find(String idProvincia) {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://sedeaplicaciones.minetur.gob.es/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        GasolineraAPI gasolineraAPI = retrofit.create(GasolineraAPI.class);
-        Call<Gasolinera> call = gasolineraAPI.obtenerGasolinerasPorProvincia(idProvincia);
-        call.enqueue(new Callback<Gasolinera>() {
-            @Override
-            public void onResponse(Call<Gasolinera> call, Response<Gasolinera> response) {
-                if (response.isSuccessful()) {
-                    Gasolinera gasolinera = response.body();
-                    List<Gasolinera> gasolinerasList = gasolinera.getListaGasolineras();
-
-                    if (gasolinerasList != null) {
-                        for (Gasolinera gasolineraItem : gasolinerasList) {
-                            double latitud = Double.parseDouble(gasolineraItem.getLatitud());
-                            double longitud = Double.parseDouble(gasolineraItem.getLongitud());
-                            String localidad = gasolineraItem.getLocalidad();
-
-                            LatLng gasolineraLatLng = new LatLng(latitud, longitud);
-                            mMap.addMarker(new MarkerOptions().position(gasolineraLatLng).title(localidad));
-                        }
-                        Toast.makeText(MainActivity.this, "Marcadores agregados al mapa", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "No se encontraron datos de gasolineras", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(MainActivity.this, "Error en la respuesta del servidor: " + response.message(), Toast.LENGTH_SHORT).show();
-                    Log.e("API_ERROR", "Error en la respuesta del servidor: " + response.message());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Gasolinera> call, Throwable t) {
-
-            }
-
-
-        });
-
-    }
 }
